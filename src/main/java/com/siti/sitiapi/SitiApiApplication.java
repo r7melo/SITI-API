d91cc7e8f0aaab838c2b0f9da5316cf81a83d356
Romario Melo <romariomelo360@gmail.com>
2026-04-01 23:32:24 -0300

    Create project api/java

---
+++ src/main/java/com/siti/sitiapi/SitiApiApplication.java
@@ -0,0 +1,13 @@
+package com.siti.sitiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siti/sitiapi/SitiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siti.sitiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
